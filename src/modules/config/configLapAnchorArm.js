eb2371dcb7d06e83fb11e0adbf5f4130c5d52de6
Sergei Manin <manin.serg@gmail.com>
2023-02-04 12:29:22 +0300

    Add config and form to html and select from table lap and anchor

---
+++ src/modules/config/configLapAnchorArm.js
@@ -0,0 +1,6 @@
+export const configLapAnchorArm = {
+    nameClassBeton: 'class-beton',
+    nameClassArm: 'class-arm',
+    nameDiameterArm: 'diameter-arm',
+    btnCalculateLapAnchor: 'btn-calculate-lap-anchor'
+}
--- src/modules/functions/calculateLapAnchorArm.js
+++ src/modules/functions/calculateLapAnchorArm.js
@@ -1,3 +1,13 @@
-export function calculateLapAnchorArm () {
-    
+export function calculateLapAnchorArm (table, config) {
+    const selectClassBeton = document.querySelector(`[name=${config.nameClassBeton}]`);
+    const selectClassArm = document.querySelector(`[name=${config.nameClassArm}]`);
+    const selectDiameterArm = document.querySelector(`[name=${config.nameDiameterArm}]`);
+    const btnCalculateLapAnchor = document.querySelector(`.${config.btnCalculateLapAnchor}`)
+
+    btnCalculateLapAnchor.addEventListener('click', e =>{
+
+        const lap = table[selectClassBeton.value][selectClassArm.value][selectDiameterArm.value]['lap'];
+        const anchor = table[selectClassBeton.value][selectClassArm.value][selectDiameterArm.value]['anchor'];
+
+    })
 }
