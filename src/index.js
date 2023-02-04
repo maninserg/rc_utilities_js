12024a91ff683cfac861bf1cf7409e2a4894bc29
Sergei Manin <manin.serg@gmail.com>
2023-02-04 12:24:30 +0300

    Add form of  selects source values for calculate lap and anchor arm

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { tableAreaArmByStep } from './modules/tables/tableAreaArmByStep';
 import { configAreaArmByCount } from './modules/config/configAreaArmByCount';
 import { configAreaArmByStep } from './modules/config/configAreaArmByStep';
 import { tableLapAnchorArm } from './modules/tables/tableLapAnchorArm';
+import { configLapAnchorArm} from './modules/config/configLapAnchorArm'; 
 import { calculateAreaArmByCountOrStep } from './modules/functions/calculateAreaArmByCountOrStep';
 import { calculateLapAnchorArm } from './modules/functions/calculateLapAnchorArm';
 
@@ -12,3 +13,5 @@ import { calculateLapAnchorArm } from './modules/functions/calculateLapAnchorArm
 calculateAreaArmByCountOrStep(configAreaArmByCount, tableAreaArmByCount);
 
 calculateAreaArmByCountOrStep(configAreaArmByStep, tableAreaArmByStep);
+
+calculateLapAnchorArm(tableLapAnchorArm, configLapAnchorArm);
