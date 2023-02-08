59fc0e3419c3ad2abadbb35992800d0e6d4297aa
Sergei Manin <manin.serg@gmail.com>
2023-02-08 21:30:36 +0300

    Settings up bootstrap elements

--- src/modules/config/configLapAnchorArm.js
+++ src/modules/config/configLapAnchorArm.js
@@ -2,5 +2,10 @@ export const configLapAnchorArm = {
     nameClassBeton: 'class-beton',
     nameClassArm: 'class-arm',
     nameDiameterArm: 'diameter-arm',
-    btnCalculateLapAnchor: 'btn-calculate-lap-anchor'
+    btnCalculateLapAnchor: 'btn-calculate-lap-anchor',
+    spanResultAnchor: 'span-result-anchor',
+    spanResultLap: 'span-result-lap',
+    arrowPlay: 'arrow-play-lap-anchor',
+    arrowPause: 'arrow-pause-lap-anchor'
+
 }
--- src/modules/functions/calculateLapAnchorArm.js
+++ src/modules/functions/calculateLapAnchorArm.js
@@ -1,13 +1,53 @@
+"use strict";
+
 export function calculateLapAnchorArm (table, config) {
     const selectClassBeton = document.querySelector(`[name=${config.nameClassBeton}]`);
     const selectClassArm = document.querySelector(`[name=${config.nameClassArm}]`);
     const selectDiameterArm = document.querySelector(`[name=${config.nameDiameterArm}]`);
-    const btnCalculateLapAnchor = document.querySelector(`.${config.btnCalculateLapAnchor}`)
+    const btnCalculateLapAnchor = document.querySelector(`.${config.btnCalculateLapAnchor}`);
+
+    const arrowPlayDiv = document.querySelector(`.${config.arrowPlay}`);
+    const arrowPauseDiv = document.querySelector(`.${config.arrowPause}`);
+
+    const spanResultAnchor = document.querySelector(`.${config.spanResultAnchor}`);
+    const spanResultLap = document.querySelector(`.${config.spanResultLap}`);
 
     btnCalculateLapAnchor.addEventListener('click', e =>{
 
-        const lap = table[selectClassBeton.value][selectClassArm.value][selectDiameterArm.value]['lap'];
-        const anchor = table[selectClassBeton.value][selectClassArm.value][selectDiameterArm.value]['anchor'];
+        try {
+            const lap = table[selectClassBeton.value][selectClassArm.value][selectDiameterArm.value]['lap'];
+            console.log(lap)
+            const anchor = table[selectClassBeton.value][selectClassArm.value][selectDiameterArm.value]['anchor'];
+
+            spanResultAnchor.style.color = 'blue';
+            spanResultAnchor.innerHTML = anchor;
+            spanResultLap.style.color = 'blue';
+            spanResultLap.innerHTML = lap;
+
+            arrowPlayDiv.style.display = 'inline';
+            arrowPauseDiv.style.display = 'none';
+
+        } catch (err) {
+            arrowPlayDiv.style.display = 'none';
+            arrowPauseDiv.style.display = 'inline';
+            spanResultAnchor.style.color = 'red';
+            spanResultAnchor.innerHTML = '0';
+            spanResultLap.style.color = 'red';
+            spanResultLap.innerHTML = '0';
+            console.log(err);
+        }
+
+
+/*
+
+        if (valueClassBeton !== 'Выберите класс бетона' || valueClassArm !== 'Выберите класса арматуры' || valueDiameterArm !== 'Выберите диаметр арматуры') {
+            const lap = table[selectClassBeton.value][selectClassArm.value][selectDiameterArm.value]['lap'];
+            const anchor = table[selectClassBeton.value][selectClassArm.value][selectDiameterArm.value]['anchor'];
+    
+            spanResultAnchor.innerHTML = anchor;
+            spanResultLap.innerHTML = lap;
+        }
+*/
 
     })
 }
